4fa6d4598ba934d0f92ff40a2f2b20df51a95de5
Gil-Katzir <gilikatzir2@gmail.com>
2026-04-06 12:08:25 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -593,6 +593,7 @@ def show_chart1():
                 st.rerun()
     else:
         drill_df = month_daily_totals(st.session_state.chart1_month)
+
         fig = px.line(
             drill_df,
             x="Day",
@@ -600,7 +601,7 @@ def show_chart1():
             markers=True,
             color_discrete_sequence=['#60a5fa']
         )
-        
+
         fig = apply_common_layout(fig, f"Daily Revenue - {st.session_state.chart1_month}")
         st.plotly_chart(fig, use_container_width=True)
 
